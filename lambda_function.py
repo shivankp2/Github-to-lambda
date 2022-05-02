d9aff2c82769fe2c6388adcc70a2de76df6bb50e
shivankp2 <shivankp2@users.noreply.github.com>
2022-05-02 13:11:48 +0530

    Test date and time

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
-import pandas as pd
-
-def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.1')
+import datetime
+now = datetime.datetime.now()
+print(now)
+year = lambda x: x.year
+month = lambda x: x.month
+day = lambda x: x.day
+t = lambda x: x.time()
+print(year(now))
+print(month(now))
+print(day(now))
+print(t(now))
