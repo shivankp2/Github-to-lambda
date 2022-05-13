19671f3d2274e210e4c14103905f3737402cec68
shivankp2 <shivankp2@users.noreply.github.com>
2022-05-14 00:32:27 +0530

    External API call 2

--- lambda_function.py
+++ lambda_function.py
@@ -8,8 +8,6 @@ from datetime import datetime
 
 def lambda_handler(event, context):
     now = datetime.now()
-    now_str = now.strftime(‘%Y-%m-%d %H:%M:%S’)
-
     response = requests.get(‘https://newsapi.org/v2/everything?q=tesla&from=2022-05-07&sortBy=publishedAt&apiKey=590a0141d1e44a268a21f6bb6c39d5ea′)
 
     if response.status_code == 200:
