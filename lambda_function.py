741fc4bda09557d9cba6f7d5fc0977ff7a0a494c
shivankp2 <shivankp2@users.noreply.github.com>
2022-05-14 00:29:14 +0530

    External API call 2

--- lambda_function.py
+++ lambda_function.py
@@ -7,23 +7,19 @@ from botocore.vendored import requests
 from datetime import datetime
 
 def lambda_handler(event, context):
+    now = datetime.now()
+    now_str = now.strftime(‘%Y-%m-%d %H:%M:%S’)
 
-# Get the current date and time in yy–mm-dd hours:minutes:seconds format
-now = datetime.now()
-now_str = now.strftime(‘%Y-%m-%d %H:%M:%S’)
+    response = requests.get(‘https://newsapi.org/v2/everything?q=tesla&from=2022-05-07&sortBy=publishedAt&apiKey=590a0141d1e44a268a21f6bb6c39d5ea′)
 
-# API call
-response = requests.get(‘https://newsapi.org/v2/everything?q=tesla&from=2022-05-07&sortBy=publishedAt&apiKey=590a0141d1e44a268a21f6bb6c39d5ea′)
+    if response.status_code == 200:
+        datadictionary = response.json()
 
-# Check if the request completed successfully
-if response.status_code == 200:
+    for data in datadictionary[‘list’]:
+        source = articles['name']
+        author = articles['author']
+        title = articles['title']
+        publishedAt = articles['publishedAt']
 
-# Return data and print data
-
-source = articles['name']
-author = articles['author']
-title = articles['title']
-publishedAt = articles['publishedAt']
-
-print(f."Name:{name}, author :{author}, title : {title}, publishedAt:{publishedAt}")
-return "Name:{}, author :{}, title : {}, publishedAt:{}".format(name,author,title,publishedAt))
+    print(f."Name:{name}, author :{author}, title : {title}, publishedAt:{publishedAt}")
+    return "Name:{}, author :{}, title : {}, publishedAt:{}".format(name,author,title,publishedAt))
